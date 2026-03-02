f85502acd76d31cd16c5e2645359ee0413f14372
ho <howardxsheng@gmail.com>
2026-03-02 15:49:58 +0800

    chore: add prettier config and ignore rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-
-import './index.css'
-import { ping } from '@/utils/test'
+import './index.css';
 
 function App() {
-
-
   return (
     <>
-      <h1 className='text-red-500 text-5xl'>Hello World</h1>
-      <h2 className="text-5xl text-red-500 font-bold">{ping()}</h2>
+      <h1 className="text-red-500 text-5xl">Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
