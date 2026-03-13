cb12e0aca1f37e863f463ea0ccbc02fb7c90e46e
IvyYang <ivy990030@icloud.com>
2026-03-13 13:26:32 +0800

    update: revise the css for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './assets/styles/index.css';
 
 function App() {
   return (
-    <div className="text-primary-600 mx-auto flex max-w-200 flex-col items-center justify-center gap-4 px-3 text-5xl">
+    <div className="font-display-lg text-primary-dark mx-auto flex max-w-200 flex-col items-center justify-center gap-4 px-3 text-5xl">
       <p>Seasons Care © 2026</p>
     </div>
   );
