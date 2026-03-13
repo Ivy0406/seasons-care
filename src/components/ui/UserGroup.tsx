bb1b5cc5aef3ab13306376907fcead104368db8e
IvyYang <ivy990030@icloud.com>
2026-03-14 00:56:03 +0800

    feat: create UserGroup component

---
+++ src/components/ui/UserGroup.tsx
@@ -0,0 +1,44 @@
+import { Children, isValidElement } from 'react';
+import type { ReactNode } from 'react';
+
+import { ChevronDown } from 'lucide-react';
+
+import cn from '@/lib/utils';
+
+import { AvatarGroup } from './avatar';
+
+type DynamicUserGroupProps = {
+  children: ReactNode;
+};
+
+function DynamicUserGroup({ children }: DynamicUserGroupProps) {
+  const childArray = Children.toArray(children).filter(isValidElement);
+  const count = childArray.length;
+
+  let spacingClass: string = '-space-x-2';
+  if (count === 3) {
+    spacingClass = '-space-x-3';
+  } else if (count > 3) {
+    spacingClass = '-space-x-4';
+  }
+
+  return (
+    <div className="flex h-11 w-full max-w-25 items-center justify-between rounded-full bg-neutral-800 p-2">
+      <AvatarGroup
+        className={cn(
+          'flex w-fit flex-row-reverse justify-end space-x-reverse',
+          spacingClass,
+        )}
+      >
+        {children}
+      </AvatarGroup>
+      <ChevronDown className="text-neutral-100" size={16} strokeWidth={2.5} />
+    </div>
+  );
+}
+
+function UserGroup({ children }: { children?: ReactNode }) {
+  return <DynamicUserGroup>{children}</DynamicUserGroup>;
+}
+
+export default UserGroup;
