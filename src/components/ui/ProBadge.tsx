77699e17219c2a5abfe13125df038b82aee4d4d2
IvyYang <ivy990030@icloud.com>
2026-03-13 13:22:24 +0800

    update: remove React.FC type from ProBadge component

--- src/components/ui/ProBadge.tsx
+++ src/components/ui/ProBadge.tsx
@@ -1,4 +1,4 @@
-const ProBadge: React.FC = () => {
+const ProBadge = () => {
   return (
     <div className="font-label-sm flex h-[25px] w-8 items-center justify-center rounded-full bg-neutral-800 px-1 py-0.5 text-neutral-50">
       <p>Pro</p>
