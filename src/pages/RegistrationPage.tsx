43323a81bf7d9430e12f6fb1f874c3cc4c5e5c9b
IvyYang <ivy990030@icloud.com>
2026-03-17 16:49:22 +0800

    feat: add RegistrationPage

---
+++ src/pages/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '@/features/auth/components/RegisterForm';
+
+function RegistrationPage() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default RegistrationPage;
