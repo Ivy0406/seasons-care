8f0de9189e84e0a51f782f10162fa89f64b98a79
IvyYang <ivy990030@icloud.com>
2026-03-17 14:37:31 +0800

    feat: add routes to login page and registration page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router';
+
 import {
   RoundedButtonPrimary,
   RoundedButtonSecondary,
@@ -5,12 +7,18 @@ import {
 import { LandingKeyVisual } from '@/features/auth';
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-15">
       <LandingKeyVisual />
       <div className="flex w-full flex-col items-center gap-3">
-        <RoundedButtonPrimary>登入</RoundedButtonPrimary>
-        <RoundedButtonSecondary>註冊</RoundedButtonSecondary>
+        <RoundedButtonPrimary onClick={() => navigate('login')}>
+          登入
+        </RoundedButtonPrimary>
+        <RoundedButtonSecondary onClick={() => navigate('registration')}>
+          註冊
+        </RoundedButtonSecondary>
       </div>
     </div>
   );
