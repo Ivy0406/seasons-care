c6182f064284a4e769208f0fcd92cd7a523e8f51
IvyYang <ivy990030@icloud.com>
2026-03-17 22:32:22 +0800

    feat: add SetupProfile export

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as LandingKeyVisual } from './components/LandingKeyVisual';
-export { default as RegisterForm } from './components/RegisterForm';
+export { default as RegisterAccount } from './components/RegisterAccount';
+export { default as SetupProfile } from './components/SetupProfile';
