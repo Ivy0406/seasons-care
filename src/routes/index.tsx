7bb893c239bd04cef6c0ad687b605401d5a8d44d
IvyYang <ivy990030@icloud.com>
2026-03-18 09:28:59 +0800

    feat: add route for OnboardingPage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createHashRouter } from 'react-router';
 import App from '@/App';
 import LandingPage from '@/pages/LandingPage';
 import LoginPage from '@/pages/LoginPage';
+import OnboardingPage from '@/pages/OnboardingPage';
 import RegistrationPage from '@/pages/RegistrationPage';
 
 const router = createHashRouter([
@@ -21,6 +22,10 @@ const router = createHashRouter([
       {
         path: 'registration',
         Component: RegistrationPage,
+      },
+      {
+        path: 'onboarding',
+        Component: OnboardingPage,
       },
     ],
   },
