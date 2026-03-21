c39e1e287d3dafd99c3610ab9ac7f391c75bf4c0
IvyYang <ivy990030@icloud.com>
2026-03-21 17:45:29 +0800

    update: import health data via useHealth hook

--- src/features/health/HealthSummary.tsx
+++ src/features/health/HealthSummary.tsx
@@ -1,6 +1,7 @@
 import { Plus } from 'lucide-react';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -14,48 +15,22 @@ import {
   DataCardBloodSugar,
 } from '@/components/common/DataCard';
 
+import useHealth from './useHealth';
+
 const swiperConfig = {
   spaceBetween: 16,
   slidesPerView: 1,
   pagination: {
     el: '.custom-pagination',
     clickable: true,
-    renderBullet(index: number, className: string) {
+    renderBullet(_index: number, className: string) {
       return `<span class="${className} transition-all duration-300"></span>`;
     },
   },
 };
 
-type HealthData = {
-  time: string | '';
-  summary: { content: string };
-  bloodPressure: { systolic: number | '--'; diastolic: number | '--' };
-  temperature: number | '--';
-  bloodOxygen: number | '--';
-  weight: number | '--';
-  bloodSugar: {
-    morning: number | '--';
-    noon: number | '--';
-    night: number | '--';
-  };
-};
-
-const mockHealthData: HealthData = {
-  time: '10:00',
-  summary: {
-    content:
-      '下午已完成血壓測量，數值偏高，建議傍晚減少咖啡因攝取。今日復健進度已達成 80%，再加油一點點！',
-  },
-  bloodPressure: { systolic: 142, diastolic: 92 },
-  temperature: 36.5,
-  bloodOxygen: 98,
-  weight: 70.1,
-  bloodSugar: { morning: 155, noon: 155, night: '--' },
-};
-
 function HealthSummary() {
-  // 之後會換成自定義 Hook
-  const healthData = mockHealthData;
+  const healthData = useHealth();
 
   const {
     time,
