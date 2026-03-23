ba5fe5ce52b5959d093f8ad2239e126b75d60da3
IvyYang <ivy990030@icloud.com>
2026-03-24 00:44:29 +0800

    feat: add small MoneyDataFormCard

---
+++ src/features/voice/components/MoneyDataFormCard.tsx
@@ -0,0 +1,18 @@
+import DataFormCard from '@/components/common/DataFormCard';
+import { MoneyDataSmallForm } from '@/components/common/SmallDataForm';
+
+function MoneyDataFormCard() {
+  return (
+    <DataFormCard
+      title="支出記錄"
+      className="bg-secondary-default"
+      toneClassName="bg-secondary-default"
+    >
+      <DataFormCard.Content>
+        <MoneyDataSmallForm className="w-full border-0 bg-neutral-50 px-0 pt-3" />
+      </DataFormCard.Content>
+    </DataFormCard>
+  );
+}
+
+export default MoneyDataFormCard;
