f6656427fdcb9ba7466b372fee5e048ee9749b25
IvyYang <ivy990030@icloud.com>
2026-03-25 17:05:40 +0800

    feat: add response interceptors

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { toast } from 'sonner';
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -14,6 +15,30 @@ apiClient.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          toast.error('登入已過期，請重新登入');
+          break;
+        case 403:
+          toast.error('權限不足，無法執行此操作');
+          break;
+        case 500:
+          toast.error('伺服器錯誤，請稍後再試');
+          break;
+        default:
+          break;
+      }
+    } else if (error.request) {
+      toast.error('網路連線異常，請確認網路狀態');
+    }
+    return Promise.reject(error);
+  },
 );
 
 export default apiClient;
