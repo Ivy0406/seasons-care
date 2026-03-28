77c7a31238227f4a47cae1188d7391b7305f7fee
IvyYang <ivy990030@icloud.com>
2026-03-28 14:27:02 +0800

    update: add MoneyPage into isFullWidthRoute

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ function App() {
     pathname === '/calendar-page' ||
     pathname === '/calendar-page/new' ||
     pathname === '/data-form' ||
-    pathname === '/health-report';
+    pathname === '/health-report' ||
+    pathname === '/money';
 
   return (
     <div
