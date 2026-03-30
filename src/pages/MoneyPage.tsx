ff52a7bef0c4200b7d7cf312420aa0cfeefa644a
IvyYang <ivy990030@icloud.com>
2026-03-30 10:40:07 +0800

    update: import CardsList and useExpense

--- src/pages/MoneyPage.tsx
+++ src/pages/MoneyPage.tsx
@@ -1,13 +1,18 @@
 import { NavigationHeaderRow } from '@/components/common/NavigationBar';
+import CardsList from '@/features/money/components/CardsList';
 import MoneyTabsCard from '@/features/money/components/MoneyTabsCard';
+import useExpenses from '@/features/money/hooks/useExpenses';
 
 function MoneyPage() {
+  const { expenses } = useExpenses();
+
   return (
-    <main className="flex min-h-screen w-full flex-col text-neutral-900">
+    <main className="flex min-h-screen w-full flex-col bg-neutral-200 text-neutral-900">
       <NavigationHeaderRow title="帳目" className="px-6" />
       <div className="pt-2">
         <MoneyTabsCard />
       </div>
+      <CardsList items={expenses} />
     </main>
   );
 }
