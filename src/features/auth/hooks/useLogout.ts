cdd1470c57157b8b6479e931a788fda9319f8e4d
IvyYang <ivy990030@icloud.com>
2026-04-08 18:05:52 +0800

    refactor: change import method

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 import Cookie from 'js-cookie';
 
-import TOKEN_KEY from '@/constants/auth';
+import { TOKEN_KEY } from '@/constants/auth';
 
 function useLogout() {
   const navigate = useNavigate();
