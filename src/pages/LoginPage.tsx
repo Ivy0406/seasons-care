07306d0de0b092e6aaf3325f6ddaa991a7200cee
IvyYang <ivy990030@icloud.com>
2026-04-11 21:27:29 +0800

    style: add mx-auto of LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/features/auth/components/LoginForm';
 function LoginPage() {
   return (
     <main className="w-full bg-neutral-100">
-      <div className="flex h-screen w-full max-w-200 items-center justify-center px-6">
+      <div className="mx-auto flex h-screen w-full max-w-200 items-center justify-center px-6">
         <LoginForm />
       </div>
     </main>
