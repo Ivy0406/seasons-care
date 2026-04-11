71b90aee68398802766dc6163b72c56bdbbac0f6
IvyYang <ivy990030@icloud.com>
2026-04-11 21:21:10 +0800

    style: adjust bg-color and padding of LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,11 @@ import LoginForm from '@/features/auth/components/LoginForm';
 
 function LoginPage() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <LoginForm />
-    </div>
+    <main className="w-full bg-neutral-100">
+      <div className="flex h-screen w-full max-w-200 items-center justify-center px-6">
+        <LoginForm />
+      </div>
+    </main>
   );
 }
 
