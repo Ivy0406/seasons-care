05bebf93958240696f3a99f6497383744fc3de9b
IvyYang <ivy990030@icloud.com>
2026-04-20 11:14:34 +0800

    style: refine landing page layout and spacing

--- src/features/auth/components/LandingKeyVisual.tsx
+++ src/features/auth/components/LandingKeyVisual.tsx
@@ -2,14 +2,16 @@ import LandingIcon from '@/assets/icons/landing-icon.svg?react';
 
 function LandingKeyVisual() {
   return (
-    <div className="flex h-fit w-full flex-col items-center justify-between">
+    <div className="flex h-fit max-h-71 w-full flex-col items-center justify-between">
       <div className="flex flex-col items-center justify-center">
         <LandingIcon />
-        <h1 className="font-display-md text-neutral-800">蒔歲</h1>
       </div>
-      <div className="flex w-fit flex-col items-center gap-3">
-        <p className="font-heading-sm text-neutral-900">細心灌溉每一份日常</p>
-        <p className="font-heading-sm text-neutral-900">讓陪伴緩緩紮根</p>
+      <div className="flex w-fit flex-col items-center gap-7">
+        <h1 className="font-display-md text-neutral-800">蒔歲</h1>
+        <div className="flex w-fit flex-col items-center gap-1">
+          <p className="font-heading-sm text-neutral-900">細心灌溉每一份日常</p>
+          <p className="font-heading-sm text-neutral-900">讓陪伴緩緩紮根</p>
+        </div>
       </div>
       <div />
     </div>
