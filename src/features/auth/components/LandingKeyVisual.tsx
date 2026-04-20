20ec0a8c9492ae06c4f5018077c54e493d7eacff
IvyYang <ivy990030@icloud.com>
2026-04-20 10:57:13 +0800

    feat: update landing page visual style and icons

--- src/features/auth/components/LandingKeyVisual.tsx
+++ src/features/auth/components/LandingKeyVisual.tsx
@@ -1,18 +1,19 @@
-import BrandMark from '@/components/common/BrandMark';
+import LandingIcon from '@/assets/icons/landing-icon.svg?react';
 
-function Landing() {
+function LandingKeyVisual() {
   return (
-    <div className="flex h-fit w-full flex-col items-center justify-center gap-15">
-      <div className="flex w-fit flex-col items-center gap-3">
-        <p className="font-heading-lg text-neutral-900">細心灌溉每一份日常</p>
-        <p className="font-heading-lg text-neutral-900">讓陪伴緩緩紮根</p>
+    <div className="flex h-fit w-full flex-col items-center justify-between">
+      <div className="flex flex-col items-center justify-center">
+        <LandingIcon />
+        <h1 className="font-display-md text-neutral-800">蒔歲</h1>
       </div>
-      <BrandMark
-        detailClassName="text-primary-default"
-        className="h-full max-h-[89px] w-full max-w-[161px]"
-      />
+      <div className="flex w-fit flex-col items-center gap-3">
+        <p className="font-heading-sm text-neutral-900">細心灌溉每一份日常</p>
+        <p className="font-heading-sm text-neutral-900">讓陪伴緩緩紮根</p>
+      </div>
+      <div />
     </div>
   );
 }
 
-export default Landing;
+export default LandingKeyVisual;
